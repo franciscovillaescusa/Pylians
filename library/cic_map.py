c6ed9399ceb71493798d30f020066e76073ea5e5
franciscovillaescusa <villaescusa.francisco@gmail.com>
2014-03-08 16:58:57 +0100

    prueba

--- library/cic_map.py
+++ library/cic_map.py
@@ -3,11 +3,13 @@
 import numpy as np
 import sys
 
-if len(sys.argv)==1 or (len(sys.argv)!=6 and len(sys.argv)!=7):
+if len(sys.argv)==1 or (len(sys.argv)!=8 and len(sys.argv)!=9):
     print 'USAGE:'
-    print 'cic_map.py filename axis1 axis2 points_axis1 points_axis2'
+    print 'cic_map.py filename axis1 axis2 points_axis1 points_axis2 log_x log_y'
     print 'or, if overdensity plots want to be made:'
-    print 'cic_map.py filename axis1 axis2 points_axis1 points_axis2 background density'
+    print 'cic_map.py filename axis1 axis2 points_axis1 points_axis2 log_x log_y background density'
+    print 'set log_x to 1 for results in log scale in x, otherwise set to 0'
+    print 'set log_y to 1 for results in log scale in y, otherwise set to 0'
     sys.exit()
 
 fname=str(sys.argv[1])
@@ -15,26 +17,28 @@ axis_x=int(sys.argv[2])
 axis_y=int(sys.argv[3])
 points_x=int(sys.argv[4])
 points_y=int(sys.argv[5])
-if len(sys.argv)==7:
-    background_density=float(sys.argv[6])
+log_x=bool(sys.argv[6])
+log_y=bool(sys.argv[7])
+if len(sys.argv)==9:
+    background_density=float(sys.argv[8])
 
-x,y=[],[]
-f=open(fname,'r')
+
+print 'Reading the data file'
+f=open(fname,'r'); x,y=[],[]
 for line in f.readlines():
     a=line.split()
     x.append(float(a[axis_x]))
     y.append(float(a[axis_y]))
-f.close()
-x=np.array(x)
-y=np.array(y)
+f.close(); x=np.array(x); y=np.array(y)
+if log_x:
+    x=np.log10(x)
+if log_y:
+    y=np.log10(y)
 
-min_x=np.min(x)
-max_x=np.max(x)
-min_y=np.min(y)
-max_y=np.max(y)
+min_x=np.min(x); max_x=np.max(x)
+min_y=np.min(y); max_y=np.max(y)
 
-delta_x=(max_x-min_x)*1.0/points_x
-delta_y=(max_y-min_y)*1.0/points_y
+delta_x=(max_x-min_x)*1.0/points_x; delta_y=(max_y-min_y)*1.0/points_y
 area=delta_x*delta_y
 
 bin_x=((x+delta_x/2.0-min_x)/delta_x).astype(np.int64)
@@ -48,6 +52,7 @@ ty=delta_y-dy
 
 grid=np.zeros((points_x,points_y),dtype=np.float64)
 
+print 'Counting the number of points in each grid cell'
 for i in range(len(x)):
     if bin_x[i]==0:
         if bin_y[i]==0:
@@ -78,15 +83,20 @@ for i in range(len(x)):
         grid[bin_x[i]-1,bin_y[i]]+=tx[i]*dy[i]/area
         grid[bin_x[i]-1,bin_y[i]-1]+=tx[i]*ty[i]/area
 
-if len(sys.argv)==7:
+if len(sys.argv)==9:
     background_density=background_density*area
 
+print 'write the output file'
 f=open(fname+'_cic_map','w')
 for i in range(points_x):
     x_middle=min_x+(max_x-min_x)*(i+0.5)/points_x
+    if log_x:
+        x_middle=10**(x_middle)
     for j in range(points_y):
         y_middle=min_y+(max_y-min_y)*(j+0.5)/points_y
-        if len(sys.argv)==6:
+        if log_y:
+            y_middle=10**(y_middle)
+        if len(sys.argv)==8:
             f.write(str(x_middle)+' '+str(y_middle)+' '+str(grid[i,j])+'\n')
         else:
             f.write(str(x_middle)+' '+str(y_middle)+' '+str(grid[i,j]/background_density)+'\n')
