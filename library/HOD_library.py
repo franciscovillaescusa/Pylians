f91e5ebf59d40fe4cef53d91b3e48a882004536f
franciscovillaescusa <villaescusa.francisco@gmail.com>
2014-07-13 17:01:44 +0200

    HOD_fast routine now returns the velocities of the galaxies

--- library/HOD_library.py
+++ library/HOD_library.py
@@ -227,169 +227,191 @@ def hod(snapshot_fname,groups_fname,groups_number,min_mass,max_mass,
 #alpha trying to find the best combination that reproduces the measured wp(r)
 #VARIABLES:
 #DM_pos: array containing the positions of the CDM particles
+#DM_vel: array containing the velocities of the CDM particles
 #sorted_ids: array containing the positions of the IDs in the snapshots. 
 #sorted_ids[N] gives the position where the particle whose ID is N is located
 #IDs:IDs array as read from the subfind ID file
 #halo_mass: array containing the masses of the CDM halos in the mass interval
 #halo_pos:  array containing the positions of the CDM halos in the mass interval
+#halo_vel:  array with the velocities of the CDM halos in the mass interval
 #halo_radius: array containing the radii of the CDM halos in the mass interval
 #halo_len: array containing the len of the CDM halos in the mass interval
 #halo_offset: array containing the offset of the CDM halos in the mass interval
 #BoxSize: Size of the simulation Box. In Mpc/h
 #fiducial_density: galaxy number density to be reproduced, in (h/Mpc)^3
-def hod_fast(DM_pos,sorted_ids,IDs,halo_mass,halo_pos,halo_radius,halo_len,
-             halo_offset,BoxSize,min_mass,max_mass,fiducial_density,
-             M1,alpha,seed,verbose=False):
+#model: 'standard' or 'LBG'. The former in the normal one whereas the latter is
+#for the LBG HOD model which do not assign central galaxies to the halos
+#NEW!!! In the latest version this routine requires both the positions and 
+#velocities of the CDM particles. It will also return the galaxies velocities
+class hod_fast:
+    def __init__(self,DM_pos,DM_vel,sorted_ids,IDs,halo_mass,halo_pos,halo_vel,
+                 halo_radius,halo_len,halo_offset,BoxSize,min_mass,max_mass,
+                 fiducial_density,M1,alpha,seed,model='standard',
+                 verbose=False):
 
-    problematic_cases=0 #number of problematic cases (e.g. halos with Rvir=0.0)
-    thres=1e-3 #controls the max relative error to accept a galaxy density
+        problematic_cases=0 #number of problematic cases (halos with Rvir=0 ...)
+        thres=1e-3 #controls the max relative error to accept a galaxy density
 
-    ##### COMPUTE Mmin GIVEN M1 & alpha #####
-    i=0; max_iterations=20 #maximum number of iterations
-    Mmin1=min_mass; Mmin2=max_mass
-    while (i<max_iterations):
-        Mmin=0.5*(Mmin1+Mmin2) #estimation of the HOD parameter Mmin
+        ##### COMPUTE Mmin GIVEN M1 & alpha #####
+        i=0; max_iterations=20 #maximum number of iterations
+        Mmin1=min_mass; Mmin2=max_mass
+        while (i<max_iterations):
+            #estimation of the HOD parameter Mmin
+            Mmin=10**(0.5*(np.log10(Mmin1)+np.log10(Mmin2))) 
 
-        total_galaxies=0
-        inside=np.where(halo_mass>Mmin)[0]
-        mass=halo_mass[inside] #only halos with M>Mmin have central/satellites
+            total_galaxies=0
 
-        total_galaxies=mass.shape[0]+np.sum((mass/M1)**alpha)
-        mean_density=total_galaxies*1.0/BoxSize**3
-
-        if (np.absolute((mean_density-fiducial_density)/fiducial_density)<thres):
-            i=max_iterations
-        elif (mean_density>fiducial_density):
-            Mmin1=Mmin
-        else:
-            Mmin2=Mmin
-        i+=1
-
-    if verbose:
-        print ' '
-        print 'Mmin=',Mmin
-        print 'average number of galaxies=',total_galaxies
-        print 'average galaxy density=',mean_density
-    #########################################
-
-    #just halos with M>Mmin; the rest do not host central/satellite galaxies
-    inside=np.where(halo_mass>Mmin)[0]
-    halo_mass=halo_mass[inside]
-    halo_pos=halo_pos[inside]
-    halo_radius=halo_radius[inside]
-    halo_len=halo_len[inside]
-    halo_offset=halo_offset[inside]
-    del inside
-
-    #compute number of satellites in each halo using the Poisson distribution 
-    np.random.seed(seed) #this is just to check convergence on w_p(r_p)
-    N_mean_sat=(halo_mass/M1)**alpha #mean number of satellites
-    N_sat=np.empty(len(N_mean_sat),dtype=np.int32)
-    for i in range(len(N_sat)):
-        N_sat[i]=np.random.poisson(N_mean_sat[i])
-    N_tot=np.sum(N_sat)+len(halo_mass) #total number of galaxies in the catalogue
-
-    if verbose:
-        print ' '
-        print np.min(halo_mass),'< M_halo <',np.max(halo_mass)
-        print 'total number of galaxies=',N_tot
-        print 'galaxy number density=',N_tot/BoxSize**3
-
-    #put satellites following the distribution of dark matter in groups
-    if verbose:
-        print ' '
-        print 'Creating mock catalogue ...',
-
-    pos_galaxies=np.empty((N_tot,3),dtype=np.float32)
-    #index: variable that go through halos (may be several galaxies in a halo)
-    #i: variable that go through galaxies
-    #count: find number of galaxies that lie beyond its host halo virial radius
-    random.seed(seed) #this is just to check convergence on w_p(r_p)
-    index=0; count=0; i=0 
-    while (index<halo_mass.size):
-
-        position=halo_pos[index]  #position of the DM halo
-        radius=halo_radius[index] #radius of the DM halo
-
-        #save the position of the central galaxy
-        pos_galaxies[i]=position; i+=1
-
-        #if halo contains satellites, save their positions
-        Nsat=N_sat[index]
-        if Nsat>0:
-            offset=halo_offset[index] 
-            length=halo_len[index]
-            idss=sorted_ids[IDs[offset:offset+length]]
-
-            #compute the radius of those particles and keep those with R<Rvir
-            pos=DM_pos[idss]
-            posc=pos-position
-
-            #this is to populate correctly halos closer to box boundaries
-            if np.any((position+radius>BoxSize) + (position-radius<0.0)):
-                
-                inside=np.where(posc[:,0]>BoxSize/2.0)[0]
-                posc[inside,0]-=BoxSize
-                inside=np.where(posc[:,0]<-BoxSize/2.0)[0]
-                posc[inside,0]+=BoxSize
-
-                inside=np.where(posc[:,1]>BoxSize/2.0)[0]
-                posc[inside,1]-=BoxSize
-                inside=np.where(posc[:,1]<-BoxSize/2.0)[0]
-                posc[inside,1]+=BoxSize
-
-                inside=np.where(posc[:,2]>BoxSize/2.0)[0]
-                posc[inside,2]-=BoxSize
-                inside=np.where(posc[:,2]<-BoxSize/2.0)[0]
-                posc[inside,2]+=BoxSize
-                
-            radii=np.sqrt(posc[:,0]**2+posc[:,1]**2+posc[:,2]**2)
-            inside=np.where(radii<radius)[0]
-            if len(inside)<Nsat:
-                problematic_cases+=1
-                print 'problematic case',len(inside),Nsat
+            #only halos with M>Mmin have central/satellites
+            inside=np.where(halo_mass>Mmin)[0]; mass=halo_mass[inside] 
+            
+            if model=='standard': #central + satellites
+                total_galaxies=mass.shape[0]+np.sum((mass/M1)**alpha)
+            elif model=='LBG':    #only satellites
+                total_galaxies=np.sum((mass/M1)**alpha)
             else:
-                selected=random.sample(inside,Nsat)
-                pos=pos[selected]
+                print 'incorrect model'; sys.exit()
+            mean_density=total_galaxies*1.0/BoxSize**3
 
-            #aditional, not esential check. Can be comment out
-            #posc=pos-position
-            #if np.any((posc>BoxSize/2.0) + (posc<-BoxSize/2.0)):
-            #    inside=np.where(posc[:,0]>BoxSize/2.0)[0]
-            #    posc[inside,0]-=BoxSize
-            #    inside=np.where(posc[:,0]<-BoxSize/2.0)[0]
-            #    posc[inside,0]+=BoxSize
+            if (np.absolute((mean_density-fiducial_density)/fiducial_density)\
+                    <thres):
+                i=max_iterations
+            elif (mean_density>fiducial_density):
+                Mmin1=Mmin
+            else:
+                Mmin2=Mmin
+            i+=1
 
-            #    inside=np.where(posc[:,1]>BoxSize/2.0)[0]
-            #    posc[inside,1]-=BoxSize
-            #    inside=np.where(posc[:,1]<-BoxSize/2.0)[0]
-            #    posc[inside,1]+=BoxSize
+        self.Mmin=Mmin
+        if verbose:
+            print '\nMmin = %.3e'%Mmin; 
+            print 'average number of galaxies =',total_galaxies
+            print 'average galaxy density = %.3e galaxies/(Mpc/h)^3'\
+                %mean_density
+                
+        #########################################
 
-            #    inside=np.where(posc[:,2]>BoxSize/2.0)[0]
-            #    posc[inside,2]-=BoxSize
-            #    inside=np.where(posc[:,2]<-BoxSize/2.0)[0]
-            #    posc[inside,2]+=BoxSize
-            #r_max=np.max(np.sqrt(posc[:,0]**2+posc[:,1]**2+posc[:,2]**2))
-            #if r_max>radius: #check no particles beyond Rv selected 
-            #    print position
-            #    print radius
-            #    print pos
-            #    count+=1
+        #just halos with M>Mmin; the rest do not host central/satellite galaxies
+        inside=np.where(halo_mass>Mmin)[0]
+        halo_mass   = halo_mass[inside];   halo_pos = halo_pos[inside]
+        halo_radius = halo_radius[inside]; halo_len = halo_len[inside]
+        halo_offset = halo_offset[inside]; del inside
 
-                for j in range(Nsat):
-                    pos_galaxies[i]=pos[j]; i+=1
-        index+=1
+        #compute the # of satellites in each halo with the Poisson distribution 
+        np.random.seed(seed) #this is just to check convergence on w_p(r_p)
+        N_mean_sat=(halo_mass/M1)**alpha #mean number of satellites
+        N_sat=np.empty(len(N_mean_sat),dtype=np.int32)
+        for i in range(len(N_sat)):
+            N_sat[i]=np.random.poisson(N_mean_sat[i])
 
-    if verbose:
-        print 'done'
-    #some final checks
-    if i!=N_tot:
-        print 'some galaxies missing:'
-        print 'register',i,'galaxies out of',N_tot
-    if count>0:
-        print 'error:',count,'particles beyond the virial radius selected'
+        #total number of galaxies in the catalogue
+        if model=='standard': #central + satellites
+            N_tot=np.sum(N_sat)+len(halo_mass) 
+        elif model=='LBG':    #only satellites
+            N_tot=np.sum(N_sat) 
 
-    return pos_galaxies
+        self.galaxy_density=N_tot*1.0/BoxSize**3
+        if verbose:
+            print '\n%.3e < M_halo < %.3e'%(np.min(halo_mass),np.max(halo_mass))
+            print 'total number of galaxies =',N_tot
+            print 'galaxy number density = %.3e'%(N_tot/BoxSize**3)
+
+        #put satellites following the distribution of dark matter in groups
+        if verbose:
+            print '\nCreating mock catalogue ...',
+
+        pos_galaxies=np.empty((N_tot,3),dtype=np.float32)
+        vel_galaxies=np.empty((N_tot,3),dtype=np.float32)
+        #index: variable that go through halos(can be several galaxies per halo)
+        #i: variable that go through galaxies
+        #count: find # of galaxies that lie beyond its host halo virial radius
+        random.seed(seed) #this is just to check convergence on w_p(r_p)
+        index=0; count=0; i=0 
+        while (index<halo_mass.size):
+
+            position=halo_pos[index]  #position of the DM halo
+            velocity=halo_vel[index]  #velocity of the DM halo
+            radius=halo_radius[index] #radius of the DM halo
+
+            #save the position of the central galaxy
+            if model=='standard':
+                pos_galaxies[i]=position; vel_galaxies[i]=velocity; i+=1
+
+            #if halo contains satellites, save their positions
+            Nsat=N_sat[index]
+            if Nsat>0:
+                offset=halo_offset[index]; length=halo_len[index]
+                idss=sorted_ids[IDs[offset:offset+length]]
+
+                #compute the radius of the particles and keep those with R<Rvir
+                pos=DM_pos[idss]; posc=pos-position; vel=DM_vel[idss]
+
+                #this is to populate correctly halos closer to box boundaries
+                if np.any((position+radius>BoxSize) + (position-radius<0.0)):
+                
+                    inside=np.where(posc[:,0]>BoxSize/2.0)[0]
+                    posc[inside,0]-=BoxSize
+                    inside=np.where(posc[:,0]<-BoxSize/2.0)[0]
+                    posc[inside,0]+=BoxSize
+
+                    inside=np.where(posc[:,1]>BoxSize/2.0)[0]
+                    posc[inside,1]-=BoxSize
+                    inside=np.where(posc[:,1]<-BoxSize/2.0)[0]
+                    posc[inside,1]+=BoxSize
+
+                    inside=np.where(posc[:,2]>BoxSize/2.0)[0]
+                    posc[inside,2]-=BoxSize
+                    inside=np.where(posc[:,2]<-BoxSize/2.0)[0]
+                    posc[inside,2]+=BoxSize
+                
+                radii=np.sqrt(posc[:,0]**2+posc[:,1]**2+posc[:,2]**2)
+                inside=np.where(radii<radius)[0]
+                if len(inside)<Nsat:
+                    problematic_cases+=1
+                    print 'problematic case',len(inside),Nsat
+                else:
+                    selected=random.sample(inside,Nsat)
+                    pos=pos[selected]; vel=vel[selected]
+
+                #aditional, not esential check. Can be comment out
+                #posc=pos-position
+                #if np.any((posc>BoxSize/2.0) + (posc<-BoxSize/2.0)):
+                #    inside=np.where(posc[:,0]>BoxSize/2.0)[0]
+                #    posc[inside,0]-=BoxSize
+                #    inside=np.where(posc[:,0]<-BoxSize/2.0)[0]
+                #    posc[inside,0]+=BoxSize
+
+                #    inside=np.where(posc[:,1]>BoxSize/2.0)[0]
+                #    posc[inside,1]-=BoxSize
+                #    inside=np.where(posc[:,1]<-BoxSize/2.0)[0]
+                #    posc[inside,1]+=BoxSize
+
+                #    inside=np.where(posc[:,2]>BoxSize/2.0)[0]
+                #    posc[inside,2]-=BoxSize
+                #    inside=np.where(posc[:,2]<-BoxSize/2.0)[0]
+                #    posc[inside,2]+=BoxSize
+                #r_max=np.max(np.sqrt(posc[:,0]**2+posc[:,1]**2+posc[:,2]**2))
+                #if r_max>radius: #check no particles beyond Rv selected 
+                #    print position
+                #    print radius
+                #    print pos
+                #    count+=1
+
+                    for j in range(Nsat):
+                        pos_galaxies[i]=pos[j]; vel_galaxies[i]=vel[j]; i+=1
+                                              
+            index+=1
+
+        if verbose:
+            print 'done'
+        #some final checks
+        if i!=N_tot:
+            print 'some galaxies missing:\nregister %d galaxies out of %d'\
+                %(i,N_tot)
+        if count>0:
+            print 'error:',count,'particles beyond the virial radius selected'
+
+        self.pos_galaxies=pos_galaxies
+        self.vel_galaxies=vel_galaxies
 ###############################################################################
 
 
