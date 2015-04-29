826b52a638af8830795afc73d4f3bb90b77b5e06
franciscovillaescusa <villaescusa.francisco@gmail.com>
2015-04-29 19:41:11 +0200

    arguments option added

--- Mass_function/MF_theory.py
+++ Mass_function/MF_theory.py
@@ -1,21 +1,32 @@
 import numpy as np
 import mass_function_library as MFL
 import bias_library as BL
+import sys,os,imp
 
 ################################# INPUT ######################################
-M_min = 1e10 #Msun/h
-M_max = 1e16 #Msun/h
-bins  = 50
+if len(sys.argv)>1:
 
-z          = 0.0
-f_Pk_DM    = './CAMB_TABLES/ics_matterpow_0.dat'
-f_transfer = './CAMB_TABLES/ics_transfer_0.dat'
+    parameter_file = sys.argv[1]
+    if myrank==0:  print '\nLoading the parameter file ',parameter_file
+    if os.path.exists(parameter_file):
+        parms = imp.load_source("name",parameter_file)
+        globals().update(vars(parms))
+    else:   print 'file does not exists!!!'
 
-do_CDM    = False  #whether use the matter or only CDM power spectrum
-Omega_CDM = 0.2685  #set the values for do_CDM=True or do_CDM=False   
-Omega_B   = 0.0490  #set the values for do_CDM=True or do_CDM=False   
+else:
+    M_min = 1e10 #Msun/h
+    M_max = 1e16 #Msun/h
+    bins  = 50
 
-f_out = 'Crocce_MF_z=0.dat'
+    z          = 0.0
+    f_Pk_DM    = './CAMB_TABLES/ics_matterpow_0.dat'
+    f_transfer = './CAMB_TABLES/ics_transfer_0.dat'
+
+    do_CDM    = False  #whether use the matter or only CDM power spectrum
+    Omega_CDM = 0.2685 #set the values for do_CDM = True or do_CDM = False   
+    Omega_B   = 0.0490 #set the values for do_CDM = True or do_CDM = False   
+
+    f_out = 'Crocce_MF_z=0.dat'
 ##############################################################################
 OmegaM = Omega_CDM+Omega_B
 M = np.logspace(np.log10(M_min),np.log10(M_max),bins+1)
