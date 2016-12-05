ed27bbd54a48afafd25a2de7c4e2c4fb66a2165d
franciscovillaescusa <villaescusa.francisco@gmail.com>
2016-12-05 04:28:54 +0100

    updated Pk driver

--- Pk/Pk.py
+++ Pk/Pk.py
@@ -1,7 +1,5 @@
 import numpy as np
-import Power_spectrum_library as PSL
-import sys
-
+import Pk_library as PKL
 
 ################################## INPUT ######################################
 snapshot_fname = ['../ics',
@@ -14,17 +12,18 @@ snapshot_fname = ['../ics',
                   '../snapdir_006/snap_006',
                   '../snapdir_007/snap_007']
                   
-dims           = 768
+dims           = 1024
 particle_type  = [1,2]
 do_RSD         = False
 axis           = 0
 hydro          = False
+cpus           = 14
 ###############################################################################
 
 # compute the P(k) of each snapshot
 for snapshot in snapshot_fname:
-    PSL.Power_spectrum_snapshot(snapshot,dims,particle_type,
-                                do_RSD,axis,hydro)
+    PKL.Pk_Gadget(snapshot,dims,particle_type,do_RSD,axis,hydro,cpus)
+                  
 
 
 
