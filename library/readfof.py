7d67709bc25b098bef97ff4c2dd729c050ed9318
Francisco Antonio Villaescusa-Navarro <fvillaescusa@scclin001.flatironinstitute.org>
2017-03-10 15:25:06 -0500

    light modifications

--- library/readfof.py
+++ library/readfof.py
@@ -25,47 +25,47 @@ import os
 from struct import unpack
 
 class FoF_catalog:
-    def __init__(self,basedir,snapnum,long_ids=False,swap=False,SFR=False):
+    def __init__(self,basedir,snapnum,long_ids=False,swap=False,SFR=False,
+                 read_IDs=True):
 
-        if long_ids: format=np.uint64
-        else: format=np.uint32
+        if long_ids:  format = np.uint64
+        else:         format = np.uint32
 
-        exts='000'+str(snapnum)
-        exts=exts[-3:]
+        exts=('000'+str(snapnum))[-3:]
 
         #################  READ TAB FILES ################# 
-        fnb,skip=0,0 
-        Final=False
+        fnb, skip, Final = 0, 0, False
+        dt1 = np.dtype((np.float32,3))
+        dt2 = np.dtype((np.float32,6))
+        prefix = basedir+"/groups_" + exts +"/group_tab_"+exts +"."
         while not(Final):
-            fname=basedir+"/groups_" + exts +"/group_tab_"+exts +"."+str(fnb)
-            f=open(fname,'rb')
-            self.Ngroups=np.fromfile(f,dtype=np.int32,count=1)[0]
-            self.TotNgroups=np.fromfile(f,dtype=np.int32,count=1)[0]
-            self.Nids=np.fromfile(f,dtype=np.int32,count=1)[0]
-            self.TotNids=np.fromfile(f,dtype=np.uint64,count=1)[0]
-            self.Nfiles=np.fromfile(f,dtype=np.uint32,count=1)[0]
+            f=open(prefix+str(fnb), 'rb')
+            self.Ngroups    = np.fromfile(f, dtype=np.int32,  count=1)[0]
+            self.TotNgroups = np.fromfile(f, dtype=np.int32,  count=1)[0]
+            self.Nids       = np.fromfile(f, dtype=np.int32,  count=1)[0]
+            self.TotNids    = np.fromfile(f, dtype=np.uint64, count=1)[0]
+            self.Nfiles     = np.fromfile(f, dtype=np.uint32, count=1)[0]
 
-            TNG=self.TotNgroups
-            NG=self.Ngroups
+            TNG, NG = self.TotNgroups, self.Ngroups
             if fnb == 0:
-                self.GroupLen=np.empty(TNG,dtype=np.int32)
-                self.GroupOffset=np.empty(TNG,dtype=np.int32)
-                self.GroupMass=np.empty(TNG,dtype=np.float32)
-                self.GroupPos=np.empty(TNG,dtype=np.dtype((np.float32,3)))
-                self.GroupVel=np.empty(TNG,dtype=np.dtype((np.float32,3)))
-                self.GroupTLen=np.empty(TNG,dtype=np.dtype((np.float32,6)))
-                self.GroupTMass=np.empty(TNG,dtype=np.dtype((np.float32,6)))
-                if SFR:
-                    self.GroupSFR=np.empty(TNG,dtype=np.float32)
+                self.GroupLen    = np.empty(TNG, dtype=np.int32)
+                self.GroupOffset = np.empty(TNG, dtype=np.int32)
+                self.GroupMass   = np.empty(TNG, dtype=np.float32)
+                self.GroupPos    = np.empty(TNG, dtype=dt1)
+                self.GroupVel    = np.empty(TNG, dtype=dt1)
+                self.GroupTLen   = np.empty(TNG, dtype=dt2)
+                self.GroupTMass  = np.empty(TNG, dtype=dt2)
+                if SFR:  self.GroupSFR = np.empty(TNG, dtype=np.float32)
+                    
             if NG>0:
                 locs=slice(skip,skip+NG)
-                self.GroupLen[locs]=np.fromfile(f,dtype=np.int32,count=NG)
-                self.GroupOffset[locs]=np.fromfile(f,dtype=np.int32,count=NG)
-                self.GroupMass[locs]=np.fromfile(f,dtype=np.float32,count=NG)
-                self.GroupPos[locs]=np.fromfile(f,dtype=np.dtype((np.float32,3)),count=NG)
-                self.GroupVel[locs]=np.fromfile(f,dtype=np.dtype((np.float32,3)),count=NG)
-                self.GroupTLen[locs]=np.fromfile(f,dtype=np.dtype((np.float32,6)),count=NG)
-                self.GroupTMass[locs]=np.fromfile(f,dtype=np.dtype((np.float32,6)),count=NG)
+                self.GroupLen[locs]    = np.fromfile(f,dtype=np.int32,count=NG)
+                self.GroupOffset[locs] = np.fromfile(f,dtype=np.int32,count=NG)
+                self.GroupMass[locs]   = np.fromfile(f,dtype=np.float32,count=NG)
+                self.GroupPos[locs]    = np.fromfile(f,dtype=dt1,count=NG)
+                self.GroupVel[locs]    = np.fromfile(f,dtype=dt1,count=NG)
+                self.GroupTLen[locs]   = np.fromfile(f,dtype=dt2,count=NG)
+                self.GroupTMass[locs]  = np.fromfile(f,dtype=dt2,count=NG)
                 if SFR:
                     self.GroupSFR[locs]=np.fromfile(f,dtype=np.float32,count=NG)
                 skip+=NG
@@ -78,9 +78,8 @@ class FoF_catalog:
                     self.GroupVel.byteswap(True)
                     self.GroupTLen.byteswap(True)
                     self.GroupTMass.byteswap(True)
-                    if SFR:
-                        self.GroupSFR.byteswap(True)
-
+                    if SFR:  self.GroupSFR.byteswap(True)
+                        
             curpos = f.tell()
             f.seek(0,os.SEEK_END)
             if curpos != f.tell():
@@ -89,33 +88,36 @@ class FoF_catalog:
             fnb+=1
             if fnb==self.Nfiles: Final=True
 
+
         #################  READ IDS FILES ################# 
-        fnb,skip=0,0
-        Final=False
-        while not(Final):
-            fname=basedir+"/groups_" + exts +"/group_ids_"+exts +"."+str(fnb)
-            f=open(fname,'rb')
-            Ngroups=np.fromfile(f,dtype=np.uint32,count=1)[0]
-            TotNgroups=np.fromfile(f,dtype=np.uint32,count=1)[0]
-            Nids=np.fromfile(f,dtype=np.uint32,count=1)[0]
-            TotNids=np.fromfile(f,dtype=np.uint64,count=1)[0]
-            Nfiles=np.fromfile(f,dtype=np.uint32,count=1)[0]
-            Send_offset=np.fromfile(f,dtype=np.uint32,count=1)[0]
-            if fnb==0:
-                self.GroupIDs=np.zeros(dtype=format,shape=TotNids)
-            if Ngroups>0:
-                if long_ids:
-                    IDs=np.fromfile(f,dtype=np.uint64,count=Nids)
-                else:
-                    IDs=np.fromfile(f,dtype=np.uint32,count=Nids)
-                if swap:
-                    IDs=IDs.byteswap(True)
-                self.GroupIDs[skip:skip+Nids]=IDs[:]
-                skip+=Nids
-            curpos = f.tell()
-            f.seek(0,os.SEEK_END)
-            if curpos != f.tell():
-                print "Warning: finished reading before EOF for IDs file",fnb
-            f.close()
-            fnb+=1
-            if fnb==Nfiles: Final=True
+        if read_IDs:
+
+            fnb,skip=0,0
+            Final=False
+            while not(Final):
+                fname=basedir+"/groups_" + exts +"/group_ids_"+exts +"."+str(fnb)
+                f=open(fname,'rb')
+                Ngroups=np.fromfile(f,dtype=np.uint32,count=1)[0]
+                TotNgroups=np.fromfile(f,dtype=np.uint32,count=1)[0]
+                Nids=np.fromfile(f,dtype=np.uint32,count=1)[0]
+                TotNids=np.fromfile(f,dtype=np.uint64,count=1)[0]
+                Nfiles=np.fromfile(f,dtype=np.uint32,count=1)[0]
+                Send_offset=np.fromfile(f,dtype=np.uint32,count=1)[0]
+                if fnb==0:
+                    self.GroupIDs=np.zeros(dtype=format,shape=TotNids)
+                if Ngroups>0:
+                    if long_ids:
+                        IDs=np.fromfile(f,dtype=np.uint64,count=Nids)
+                    else:
+                        IDs=np.fromfile(f,dtype=np.uint32,count=Nids)
+                    if swap:
+                        IDs=IDs.byteswap(True)
+                    self.GroupIDs[skip:skip+Nids]=IDs[:]
+                    skip+=Nids
+                curpos = f.tell()
+                f.seek(0,os.SEEK_END)
+                if curpos != f.tell():
+                    print "Warning: finished reading before EOF for IDs file",fnb
+                f.close()
+                fnb+=1
+                if fnb==Nfiles: Final=True
