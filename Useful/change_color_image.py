20bbb22fa4ce90b7b1ad846ae2f9a70136eacaf5
franciscovillaescusa <villaescusa.francisco@gmail.com>
2017-07-02 12:45:37 -0400

    added script to change figure color

---
+++ Useful/change_color_image.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy import misc
+from pylab import *
+
+################################### INPUT #####################################
+f_in  = 'slice.png'
+f_out = 'slice_new.png'
+###############################################################################
+
+# read image and create structure for the new image
+data = misc.imread('slice.png');  data2 = np.zeros_like(data)
+print 'data format =',data.shape
+
+# go to each pixel and change colors, opacity...etc
+for i in range(data.shape[0]):
+    for j in range(data.shape[1]):
+        color = data[i,j] #color = [red,green,blue,alpha]
+
+        data2[i,j,0] = color[2]
+        data2[i,j,1] = color[1]
+        data2[i,j,2] = color[0]
+        data2[i,j,3] = color[3]
+
+# save image to file
+#fig=figure()
+fig=figure(figsize=(35,20))
+axis('off')
+imshow(data2)
+savefig(f_out, bbox_inches='tight')
