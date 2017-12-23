25999e19898eef479bf4a7fe19c134112949bdb9
franciscovillaescusa <villaescusa.francisco@gmail.com>
2017-12-23 15:23:49 -0500

    moved scripts to /bin

--- library/readfof.py
+++ library/readfof.py
@@ -83,8 +83,7 @@ class FoF_catalog:
             curpos = f.tell()
             f.seek(0,os.SEEK_END)
             if curpos != f.tell():
-                print "Warning: finished reading before EOF for tab file",fnb
-                sys.exit()
+                raise Exception("Warning: finished reading before EOF for tab file",fnb)
             f.close()
             fnb+=1
             if fnb==self.Nfiles: Final=True
@@ -118,8 +117,7 @@ class FoF_catalog:
                 curpos = f.tell()
                 f.seek(0,os.SEEK_END)
                 if curpos != f.tell():
-                    print "Warning: finished reading before EOF for IDs file",fnb
-                    sys.exit()
+                    raise Exception("Warning: finished reading before EOF for IDs file",fnb)
                 f.close()
                 fnb+=1
                 if fnb==Nfiles: Final=True
