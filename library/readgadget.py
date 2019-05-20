52dd98e7049dd2f45d187679139005a676ac16ec
franciscovillaescusa <villaescusa.francisco@gmail.com>
2019-05-19 19:37:35 -0700

    added functionality to read entire blocks

--- library/readgadget.py
+++ library/readgadget.py
@@ -60,27 +60,87 @@ class header:
         self.Hubble = 100.0*np.sqrt(self.omega_m*(1.0+self.redshift)**3+self.omega_l)
 
 
-# This function reads a field in the gadget file        
-def read_field(snapshot, field, ptype):
+# This function reads a block of an individual file of a gadget snapshot
+def read_field(snapshot, block, ptype):
 
     filename, fformat = fname_format(snapshot)
     head              = header(filename)
 
     if fformat=="binary":
-        return readsnap.read_block(filename, field, parttype=ptype)
+        return readsnap.read_block(filename, block, parttype=ptype)
     else:
         prefix = 'PartType%d/'%ptype
         f = h5py.File(filename, 'r')
-        if   field=="POS ":  suffix = "Coordinates"
-        elif field=="MASS":  suffix = "Masses"
-        elif field=="ID  ":  suffix = "ParticleIDs"
-        elif field=="VEL ":  suffix = "Velocities"
-        else: raise Exception('field not implemented in readgadget!')
+        if   block=="POS ":  suffix = "Coordinates"
+        elif block=="MASS":  suffix = "Masses"
+        elif block=="ID  ":  suffix = "ParticleIDs"
+        elif block=="VEL ":  suffix = "Velocities"
+        else: raise Exception('block not implemented in readgadget!')
 
         array = f[prefix+suffix][:];  f.close()
-        if field=="VEL ":  array *= np.sqrt(head.time)
+        if block=="VEL ":  array *= np.sqrt(head.time)
 
-        if field=="POS " and array.dtype==np.float64:
+        if block=="POS " and array.dtype==np.float64:
             array = array.astype(np.float32)
 
         return array
+
+# This function reads a block from an entire gadget snapshot (all files)
+# it can read several particle types at the same time. 
+# ptype has to be a list. E.g. ptype=[1], ptype=[1,2], ptype=[0,1,2,3,4,5]
+def read_block(snapshot, block, ptype, verbose=False):
+
+    # find the format of the file and read header
+    filename, fformat = fname_format(snapshot)
+    head    = header(filename)    
+    Nall    = head.nall
+    filenum = head.filenum
+
+    # find the total number of particles to read
+    Ntotal = 0
+    for i in ptype:
+        Ntotal += Nall[i]
+
+    # find the dtype of the block
+    if   block=='POS ':  dtype=np.dtype((np.float32,3))
+    elif block=='VEL ':  dtype=np.dtype((np.float32,3))
+    elif block=='MASS':  dtype=np.float32
+    elif block=='ID  ':  dtype=read_field(filename, block, ptype[0]).dtype
+    else: raise Exception('block not implemented in readgadget!')
+
+    # define the array containing the data
+    array = np.zeros(Ntotal, dtype=dtype)
+
+    # if format is binary, use readsnap.py to read the snapshot
+    if fformat=="binary" or filenum==1:
+        offset = 0
+        for pt in ptype:
+            if fformat=="binary":
+                array[offset:offset+Nall[pt]] = \
+                    readsnap.read_block(snapshot, block, pt, verbose=verbose)    
+            else:
+                array[offset:offset+Nall[pt]] = \
+                    read_field(snapshot, block, pt)    
+            offset += Nall[pt]
+
+    # use this to read hdf5 files
+    else:
+
+        # do a loop over the different particle types
+        offset = 0
+        for pt in ptype:
+
+            # do a loop over the different files
+            for i in xrange(filenum):
+                
+                # find the name of the file to read
+                filename = '%s.%d.hdf5'%(snapshot,i)
+
+                # read number of particles in the file and read the data
+                npart = header(filename).npart
+                array[offset:offset+npart[pt]] = read_field(filename, block, pt)
+                offset += npart[pt]
+
+    if offset!=Ntotal:  raise Exception('not all particles read!!!!')
+            
+    return array
